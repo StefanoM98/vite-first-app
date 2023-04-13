124a6ce63fd6efdbad1b2eb118f8db5ad2af127f
StefanoM98 <smartino030@gmail.com>
2023-04-13 10:03:12 +0200

    Add: image in banner

--- src/components/appBanner.vue
+++ src/components/appBanner.vue
@@ -4,7 +4,7 @@ export default {
     data() {
         return {
             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, aperiam maiores soluta voluptate minima expedita, quam et officia vero esse quisquam. Ex facere adipisci expedita deleniti est dolores illo quisquam?"
-
+            
         }
     },
     methods:{
@@ -16,6 +16,7 @@ export default {
 <template>
     <h2>Banner</h2>
     <p>{{ text }}</p>
+    <img src="../assets/vue.svg" alt="">
 </template>
 
 <style>
