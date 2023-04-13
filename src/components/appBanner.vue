879db3d73d7f852997721f6d75ed24808c1181c1
StefanoM98 <smartino030@gmail.com>
2023-04-13 09:54:37 +0200

    Add: fixed appTitle

---
+++ src/components/appBanner.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: "appBanner",
+    data() {
+        return {
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, aperiam maiores soluta voluptate minima expedita, quam et officia vero esse quisquam. Ex facere adipisci expedita deleniti est dolores illo quisquam?"
+
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<template>
+    <h2>Banner</h2>
+    <p>{{ text }}</p>
+</template>
+
+<style>
+
+</style>
+
---
+++ src/components/appTitle.vue
@@ -0,0 +1,14 @@
+<script>
+    export default {
+        name: "AppTitle",
+        data() {
+            return {
+                title:"Ciao sono appTitle"
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
